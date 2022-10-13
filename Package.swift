5e55c612837706cfa3e504613eee8a74206fc15d
iankoex <iankoex@icloud.com>
2022-10-13 21:01:48 +0300

    mods

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ItemsCore",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11),
+        .tvOS(.v14),
+        .watchOS(.v7)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
