e4b98fe50fbd1be3f000be980ebbfd18e63db971
iankoex <hello@iankoex.com>
2025-01-06 22:00:14 +0300

    fixing livekit views

--- Sources/ItemsCore/Models/Pagination/PagedItems.swift
+++ Sources/ItemsCore/Models/Pagination/PagedItems.swift
@@ -31,7 +31,7 @@ public struct PagedItems: Codable, Sendable, Equatable {
         } else {
             self.items.append(contentsOf: newItems)
         }
-        self.items = self.items.uniqued()
+        self.items = Array(self.items.uniqued())
         self.totalItems = newPagedItems.totalItems
         self.currentPage = newPagedItems.currentPage
         self.totalPages = newPagedItems.totalPages
